215a41ecddf7bd823a4859316820a70acc12fe85
Maxime Allanic <maxime@allanic.me>
2025-06-21 13:28:32 +0200

    Release v1.2.13

--- install.sh
+++ install.sh
@@ -55,7 +55,7 @@ done
 
 # Repository configuration
 REPO_URL="https://github.com/maximeallanic/nivuus-shell.git"
-VERSION="1.2.12"
+VERSION="1.2.13"
 
 # Function to get latest version from GitHub
 get_latest_version() {
